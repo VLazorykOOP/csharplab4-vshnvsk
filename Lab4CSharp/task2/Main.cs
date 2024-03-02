b24a328144d3edaa883f35434fc5447f9d6b957a
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2024-03-02 19:20:04 +0200

    task #2 complete

---
+++ Lab4CSharp/task2/Main.cs
@@ -0,0 +1,115 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the first vector:");
+            uint size1 = Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the size of the second vector:");
+            uint size2 = Convert.ToUInt32(Console.ReadLine());
+
+            VectorLong vector1 = new VectorLong(size1);
+            VectorLong vector2 = new VectorLong(size2);
+
+            Console.WriteLine("\nEnter elements for the first vector:");
+            vector1.Input();
+
+            Console.WriteLine("\nEnter elements for the second vector:");
+            vector2.Input();
+
+            Console.WriteLine("\nVector 1:");
+            vector1.Display();
+
+            Console.WriteLine("\nVector 2:");
+            vector2.Display();
+
+            Console.WriteLine("\nSum of vectors:");
+            VectorLong sum = vector1 + vector2;
+            sum.Display();
+
+            Console.WriteLine("\nDifference of vectors:");
+            VectorLong difference = vector1 - vector2;
+            difference.Display();
+
+            Console.WriteLine("\nMyltiplication of vectors:");
+            VectorLong myltiplication = vector1 * vector2;
+            myltiplication.Display();
+
+            Console.WriteLine("\nRemainder from division of vectors:");
+            VectorLong remainder = vector1 % vector2;
+            remainder.Display();
+
+            Console.WriteLine("\nBitwise OR of vectors:");
+            VectorLong bitwiseOR = vector1 | vector2;
+            bitwiseOR.Display();
+
+            Console.WriteLine("\nBitwise AND of vectors:");
+            VectorLong bitwiseAND = vector1 & vector2;
+            bitwiseAND.Display();
+
+            Console.WriteLine("\nBitwise XOR of vectors:");
+            VectorLong bitwiseXOR = vector1 ^ vector2;
+            bitwiseXOR.Display();
+
+            Console.WriteLine("\nBitwise left shift of vector 1 by vector 2:");
+            VectorLong leftShift = vector1 << vector2;
+            leftShift.Display();
+
+            Console.WriteLine("\nBitwise right shift of vector 1 by vector 2:");
+            VectorLong rightShift = vector1 >> vector2;
+            rightShift.Display();
+
+            Console.WriteLine("\n\nEnter a scalar value:");
+            long scalar = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("\nVector 1 + scalar:");
+            VectorLong sumScalar = vector1 + scalar;
+            sumScalar.Display();
+
+            Console.WriteLine("\nVector 1 - scalar:");
+            VectorLong differenceScalar = vector1 - scalar;
+            differenceScalar.Display();
+
+            Console.WriteLine("\nVector 1 * scalar:");
+            VectorLong myltScalar = vector1 * scalar;
+            myltScalar.Display();
+
+            Console.WriteLine("\nVector 1 / scalar:");
+            VectorLong divisionScalar = vector1 / scalar;
+            divisionScalar.Display();
+
+            Console.WriteLine("\nVector 1 % scalar:");
+            VectorLong moduloScalar = vector1 % scalar;
+            moduloScalar.Display();
+
+            Console.WriteLine("\nVector 1 | scalar:");
+            VectorLong bitwiseORScalar = vector1 | scalar;
+            bitwiseORScalar.Display();
+
+            Console.WriteLine("Vector 1 & scalar:");
+            VectorLong bitwiseANDScalar = vector1 & scalar;
+            bitwiseANDScalar.Display();
+
+            Console.WriteLine("\nVector 1 ^ scalar:");
+            VectorLong bitwiseXORScalar = vector1 ^ scalar;
+            bitwiseXORScalar.Display();
+
+            Console.WriteLine("\nVector 1 << scalar:");
+            VectorLong leftShiftScalar = vector1 << scalar;
+            leftShiftScalar.Display();
+
+            Console.WriteLine("\nVector 1 >> scalar:");
+            VectorLong rightShiftScalar = vector1 >> scalar;
+            rightShiftScalar.Display();
+
+            Console.WriteLine("\nEquality of vectors: " + (vector1 == vector2));
+            Console.WriteLine("Inequality of vectors: " + (vector1 != vector2));
+            Console.WriteLine("Vector 1 > Vector 2: " + (vector1 > vector2));
+            Console.WriteLine("Vector 1 >= Vector 2: " + (vector1 >= vector2));
+            Console.WriteLine("Vector 1 < Vector 2: " + (vector1 < vector2));
+            Console.WriteLine("Vector 1 <= Vector 2: " + (vector1 <= vector2));
+        }
+    }
+}
---
+++ Lab4CSharp/task2/Vector.cs
@@ -0,0 +1,522 @@
+﻿using System;
+
+namespace Task2
+{
+    class VectorLong
+    {
+        private long[] IntArray;
+        private uint size;
+        private int codeError;
+        private static uint num_v1;
+
+        //Constructors
+        public VectorLong()
+        {
+            size = 1;
+            IntArray = new long[size];
+            IntArray[0] = 0;
+            num_v1++;
+        }
+
+        public VectorLong(uint size)
+        {
+            this.size = size;
+            IntArray = new long[size];
+
+            for(int i = 0; i < size; i++)
+            {
+                IntArray[i] = 0;
+            }
+
+            num_v1++;
+        }
+
+        public VectorLong(uint size, long initValue)
+        {
+            this.size = size;
+            IntArray = new long[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                IntArray[i] = initValue;
+            }
+
+            num_v1++;
+        }
+
+        //Destructor
+        ~VectorLong()
+        {
+            Console.WriteLine("Destructor");
+        }
+
+        //Methods
+        public void Input()
+        {
+            Console.WriteLine("Enter element: ");
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write($"Index {i}: ");
+                IntArray[i] = Convert.ToInt64(Console.ReadLine());
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Vector elements: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Index: {i}: \t{IntArray[i]}");
+            }
+        }
+
+        public void AssignValue(long value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                IntArray[i] = value;
+            }
+        }
+
+        public static uint CountVectors()
+        {
+            return num_v1;
+        }
+
+        //Properties
+        public uint Size
+        {
+            get { return size; }
+        }
+
+        public int CodeError
+        {
+            get { return codeError; }
+            set { codeError = value; }
+        }
+
+        //Indexer
+        public long this[int index]
+        {
+            get
+            {
+                if(index < 0 || index >= size)
+                {
+                    codeError = -1;
+                    return 0;
+                }
+                else
+                {
+                    codeError = 0;
+                    return IntArray[index];
+                }
+            }
+
+            set
+            {
+                if (index >=0 && index < size)
+                {
+                    IntArray[index] = value;
+                }
+                else
+                {
+                    codeError = -1;
+                }
+            }
+        }
+
+        //Overload
+        public static VectorLong operator ++(VectorLong vector)
+        {
+            for (int i = 0; i < vector.size; i++)
+            {
+                vector.IntArray[i]++;
+            }
+            return vector;
+        }
+
+        public static VectorLong operator --(VectorLong vector)
+        {
+            for (int i = 0; i < vector.size; i++)
+            {
+                vector.IntArray[i]--;
+            }
+            return vector;
+        }
+
+        public static bool operator true(VectorLong vector)
+        {
+            foreach(long element in vector.IntArray)
+            {
+                if(element != 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator false(VectorLong vector)
+        {
+            foreach (long element in vector.IntArray)
+            {
+                if (element == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !(VectorLong vector)
+        {
+            return vector.size != 0;
+        }
+
+        public static VectorLong operator ~(VectorLong vector)
+        {
+            for(int i = 0; i < vector.size; i++)
+            {
+                vector.IntArray[i] = ~vector.IntArray[i];
+            }
+            return vector;
+        }
+
+        //Arithmetic operations
+        // + Addition
+        public static VectorLong operator +(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for(int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) + (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator +(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for(int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] + scalar;
+            }
+
+            return result;
+        }
+
+        // - Subtraction
+        public static VectorLong operator -(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) - (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator -(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] - scalar;
+            }
+
+            return result;
+        }
+
+        // * Multiplication
+        public static VectorLong operator *(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) * (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator *(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] * scalar;
+            }
+
+            return result;
+        }
+
+        // / Division
+        public static VectorLong operator /(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) / (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator /(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] / scalar;
+            }
+
+            return result;
+        }
+
+        // % Modulo
+        public static VectorLong operator %(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) % (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator %(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] % scalar;
+            }
+
+            return result;
+        }
+
+        // | Bitwise OR
+        public static VectorLong operator |(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) | (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator |(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] | scalar;
+            }
+
+            return result;
+        }
+
+        // ^ Bitwise XOR
+        public static VectorLong operator ^(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) ^ (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator ^(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] ^ scalar;
+            }
+
+            return result;
+        }
+
+        // & Bitwise AND
+        public static VectorLong operator &(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) & (i < v2.Size ? v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator &(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] & scalar;
+            }
+
+            return result;
+        }
+
+        // >> Bitwise Right Shift
+        public static VectorLong operator >>(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) >> (i < v2.Size ? (int)v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator >>(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] >> (int)scalar;
+            }
+
+            return result;
+        }
+
+        // << Bitwise Left Shift
+        public static VectorLong operator <<(VectorLong v1, VectorLong v2)
+        {
+            uint maxSize = Math.Max(v1.size, v2.size);
+            VectorLong result = new VectorLong(maxSize);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                result[i] = (i < v1.Size ? v1[i] : 0) << (i < v2.Size ? (int)v2[i] : 0);
+            }
+
+            return result;
+        }
+
+        public static VectorLong operator <<(VectorLong v, long scalar)
+        {
+            VectorLong result = new VectorLong(v.Size);
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                result[i] = v[i] << (int)scalar;
+            }
+
+            return result;
+        }
+
+
+        // == Equality
+        public static bool operator ==(VectorLong v1, VectorLong v2) 
+        {
+            if (v1.size != v2.size)
+            {
+                return false;
+            }
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (v1[i] != v2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // != Inequality
+        public static bool operator !=(VectorLong v1, VectorLong v2)
+        {
+            return !(v1 == v2);
+        }
+
+        // > Greater than
+        public static bool operator >(VectorLong v1, VectorLong v2)
+        {
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (v1[i] <= v2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // >= Greater than or equal to
+        public static bool operator >=(VectorLong v1, VectorLong v2)
+        {
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (v1[i] < v2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // < Less than
+        public static bool operator <(VectorLong v1, VectorLong v2)
+        {
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (v1[i] >= v2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // <= Less than or equal to
+        public static bool operator <=(VectorLong v1, VectorLong v2)
+        {
+            for (int i = 0; i < v1.size; i++)
+            {
+                if (v1[i] > v2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
